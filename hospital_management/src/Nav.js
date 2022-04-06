c286fa864848688409ff4e415274d4061e122e8d
Asad Ali <50539082+asadzali89@users.noreply.github.com>
2022-04-06 17:52:26 -0500

    Works!!

--- hospital_management/src/Nav.js
+++ hospital_management/src/Nav.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
 
-
+function Nav(){
+    return(
+        <div>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+}
 
 export default Nav
