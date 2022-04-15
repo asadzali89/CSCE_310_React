acca06e37b17082e8ff6b5356f4f54baeeb95c25
davidfasina <fasinadavid@gmail.com>
2022-04-15 01:59:13 -0500

    Finalized registration page with error handling, and select options.

--- hospital_management/src/forms/RegistrationForm.js
+++ hospital_management/src/forms/RegistrationForm.js
@@ -1,208 +1,259 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-class RegistrationForm extends Component {
-  constructor() {
-    super();
+function RegistrationForm() {
 
-    this.state = {
-      fname: "",
-      lname: "",
-      dob: "",
-      gender: "",
-      street_addr: "",
-      state: "",
-      zip_code: 0,
-      email: "",
-      phone_number: "",
-      password: "",
-    };
+  const navigate = useNavigate();
+  const [fname, setFname] = useState("")
+  const [lname, setLname] = useState("")
+  const [dob, setDob] = useState("")
+  const [gender, setGender] = useState("")
+  const [street_addr, setStreet_addr] = useState("")
+  const [state, setState] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone_number, setPhone_number] = useState("")
+  const [password, setPassword] = useState("")
+  const [zip_code, setZip_code] = useState(0)
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    let target = event.target;
-    let value = target.type === "checkbox" ? target.checked : target.value;
-    let name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    // console.log("The form was submitted with the following data:");
-    // console.log(this.state);
+    const formDetails = {
+      'fname' : fname,
+      'lname' : lname,
+      'dob' : dob,
+      'gender' : gender,
+      'street_addr' : street_addr,
+      'state' : state,
+      'email' : email,
+      'phone_number': phone_number,
+      'password' : password,
+      'zip_code' : zip_code
+    }
+    console.log("The form was submitted with the following data:");
+    console.log(formDetails);
 
     const requestOptions = {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(this.state)
+      body: JSON.stringify(formDetails)
     };
-    
+
     fetch('http://localhost:3001/patients', requestOptions)
-    .then(console.log("New Patient added"))
+      .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json');
+        const data = isJson && await response.json();
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+
+        navigate('/login')
+      })
+      .catch(error => {
+          console.error('There was an error!', error);
+      });
   }
 
-  render() {
-    return (
-      <div className="formCenter">
-        <form onSubmit={this.handleSubmit} className="formFields">
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="fname">
-              First Name
-            </label>
-            <input
-              type="text"
-              id="fname"
-              className="formFieldInput"
-              placeholder="Enter your first name"
-              name="fname"
-              value={this.state.fname}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="lname">
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lname"
-              className="formFieldInput"
-              placeholder="Enter your last name"
-              name="lname"
-              value={this.state.lname}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="dob">
-              Date of Birth
-            </label>
-            <input
-              type="date"
-              id="dob"
-              className="formFieldInput"
-              name="dob"
-              value={this.state.dob}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="gender">
-              Gender
-            </label>
-            <input
-              type="text"
-              id="gender"
-              className="formFieldInput"
-              name="gender"
-              value={this.state.gender}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="street_addr">
-              Street Address
-            </label>
-            <input
-              type="text"
-              id="street_addr"
-              className="formFieldInput"
-              placeholder="Type Street Address"
-              name="street_addr"
-              value={this.state.street_addr}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="state">
-              State
-            </label>
-            <input
-              type="text"
-              id="state"
-              className="formFieldInput"
-              placeholder="Type State of Residence"
-              name="state"
-              value={this.state.state}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="zip_code">
-              Zip Code
-            </label>
-            <input
-              type="number"
-              min="0"
-              max="99999"
-              id="zip_code"
-              className="formFieldInput"
-              placeholder="77840"
-              name="zip_code"
-              value={parseInt(this.state.zip_code)}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="phone_number">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              id="phone_number"
-              className="formFieldInput"
-              placeholder="979-123-4567"
-              name="phone_number"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              value={this.state.phone_number}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="email">
-              E-Mail Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="formFieldInput"
-              placeholder="Enter your email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="formFieldInput"
-              placeholder="Enter your password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
+  return (
+    <div className="formCenter">
+      <form onSubmit={handleSubmit} className="formFields">
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="fname">
+            First Name
+          </label>
+          <input
+            type="text"
+            id="fname"
+            className="formFieldInput"
+            placeholder="Enter your first name"
+            name="fname"
+            required
+            onChange={e => setFname(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="lname">
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="lname"
+            className="formFieldInput"
+            placeholder="Enter your last name"
+            name="lname"
+            required
+            onChange={e => setLname(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="dob">
+            Date of Birth
+          </label>
+          <input
+            type="date"
+            id="dob"
+            className="formFieldInput"
+            name="dob"
+            required
+            onChange={e => setDob(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="gender">
+            Gender
+          </label>
+          <select name="gender" id="gender" className="formStateInput" onChange={e => setGender(e.target.value)}>
+            <option value="" selected="selected">Select your Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other / Do not wish to disclose</option>
+          </select>
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="street_addr">
+            Street Address
+          </label>
+          <input
+            type="text"
+            id="street_addr"
+            className="formFieldInput"
+            placeholder="Type Street Address"
+            name="street_addr"
+            required
+            onChange={e => setStreet_addr(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="state">
+            State
+          </label>
+          <select name="state" id="state" className="formStateInput" onChange={e => setState(e.target.value)}>
+            <option value="" selected="selected">Select a State</option>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+          </select>
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="zip_code">
+            Zip Code
+          </label>
+          <input
+            type="number"
+            min="0"
+            max="99999"
+            id="zip_code"
+            className="formFieldInput"
+            placeholder="77840"
+            name="zip_code"
+            required
+            onChange={e => setZip_code(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="phone_number">
+            Phone Number
+          </label>
+          <input
+            type="tel"
+            id="phone_number"
+            className="formFieldInput"
+            placeholder="979-123-4567"
+            name="phone_number"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            required
+            onChange={e => setPhone_number(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="email">
+            E-Mail Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="formFieldInput"
+            placeholder="Enter your email"
+            name="email"
+            required
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="formFieldInput"
+            placeholder="Enter your password"
+            name="password"
+            required
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
 
-          <div className="formField">
-            <button className="formFieldButton">Sign Up</button>{" "}
-            <Link to="/Login" className="formFieldLink">
-              I'm already a member
-            </Link>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        <div className="formField">
+          <button className="formFieldButton">Sign Up</button>{" "}
+          <Link to="/Login" className="formFieldLink">
+            I'm already a member
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
 export default RegistrationForm;
