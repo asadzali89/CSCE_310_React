c5ec6588f6eb74239389fe9944a6a1a8c840b49f
davidfasina <fasinadavid@gmail.com>
2022-04-19 22:48:22 -0500

    Converted Class component to function and connected login to api endpoint with a redirect to home page

--- hospital_management/src/forms/SignInForm.js
+++ hospital_management/src/forms/SignInForm.js
@@ -1,80 +1,91 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-class SignInForm extends Component {
-  constructor() {
-    super();
+function SignInForm() {
 
-    this.state = {
-      email: "",
-      password: ""
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) =>  {
+    e.preventDefault();
+    const formDetails = {
+      "email" : email,
+      "password" : password
+    }
+    console.log("The form was submitted with the following data:");
+    console.log(formDetails);
+    const requestOptions = {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formDetails) 
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    fetch('http://localhost:3001/login', requestOptions)
+      .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json');
+        const data = isJson && await response.json();
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+        console.log(data) 
+        if (data.id != null) {
+          localStorage.setItem('email', data.email)
+          navigate('/')
+        } else {
+          alert("Incorrect email and/or password. Try again")
+        }
+
+      })
+      .catch(error => {
+          console.error('There was an error!', error);
+      });
   }
 
-  handleChange(event) {
-    let target = event.target;
-    let value = target.type === "checkbox" ? target.checked : target.value;
-    let name = target.name;
+  return (
+    <div className="formCenter">
+      <form className="formFields" onSubmit={handleSubmit}>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="email">
+            E-Mail Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="formFieldInput"
+            placeholder="Enter your email"
+            name="email"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
 
-    this.setState({
-      [name]: value
-    });
-  }
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="formFieldInput"
+            placeholder="Enter your password"
+            name="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
 
-  handleSubmit(event) {
-    event.preventDefault();
-
-    console.log("The form was submitted with the following data:");
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <div className="formCenter">
-        <form className="formFields" onSubmit={this.handleSubmit}>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="email">
-              E-Mail Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="formFieldInput"
-              placeholder="Enter your email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="formFieldInput"
-              placeholder="Enter your password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="formField">
-            <button className="formFieldButton">Sign In</button>{" "}
-            <Link to="/Register" className="formFieldLink">
-              Create an account
-            </Link>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        <div className="formField">
+          <button className="formFieldButton">Sign In</button>{" "}
+          <Link to="/Register" className="formFieldLink">
+            Create an account
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SignInForm;
