c7496d8abc56416da6b2f12efb711a9a7bedb821
Asad Ali <asadzali89@tamu.edu>
2022-05-03 13:11:25 -0500

    Admins.js add user update

--- hospital_management/src/Admins.js
+++ hospital_management/src/Admins.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Modal from './forms/Modal'
+import './Appointment.css';
 
 function Admins(){
+    const [fname, setFname] = useState("")
+    const [lname, setLname] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     const [adminData, setAdminData] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [entity, setEntity] = useState([]);
@@ -19,6 +25,40 @@ function Admins(){
         // empty dependency array means this effect will only run once (like componentDidMount in classes)
       }, []);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formDetails = {
+          'fname' : fname,
+          'lname' : lname,
+          'email' : email,
+          'password' : password
+        }
+        console.log("The Admin was added successfully:");
+        console.log(formDetails);
+    
+        const requestOptions = {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formDetails)
+        };
+    
+        fetch('http://localhost:3001/admins', requestOptions)
+          .then(async response => {
+            const isJson = response.headers.get('content-type')?.includes('application/json');
+            const data = isJson && await response.json();
+    
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || response.status;
+                return Promise.reject(error);
+            }
+          })
+          .catch(error => {
+              alert('There was an error, try selecting a new unique email')
+              console.error('There was an error!', error);
+          });
+        }
         const handleDelete = (id) => {
             // DELETE request using fetch with error handling
             fetch(`http://localhost:3001/admins/${id}`, { method: 'DELETE' })
@@ -71,6 +111,69 @@ function Admins(){
             </div>
         );
         }
+        return(
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="fname">
+                            Admin First Name
+                        </label>
+                        <input
+                            type="text"
+                            id="fname"
+                            placeholder="Enter Admins first name"
+                            name="fname"
+                            required
+                            onChange={e => setFname(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="lname">
+                            Admin Last Name
+                        </label>
+                        <input
+                            type="text"
+                            id="lname"
+                            placeholder="Enter Admins last name"
+                            name="lname"
+                            required
+                            onChange={e => setLname(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">
+                            Admin email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Enter Admins email"
+                            name="email"
+                            required
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">
+                            Admin Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            placeholder="Enter Admins password"
+                            name="password"
+                            required
+                            onChange={e => setFname(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <button >Add Admin</button>{" "}
+                    </div>
+                </form>
+            </div>
+
+        );
       }
 
 export default Admins;
