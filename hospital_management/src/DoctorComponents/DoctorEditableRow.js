359cb332e0a15f89ee023c4fb156aa0900282b07
Mahirah Samah <mahirah.samah@tamu.edu>
2022-05-06 00:33:14 -0500

    changed to appt_id

--- hospital_management/src/DoctorComponents/DoctorEditableRow.js
+++ hospital_management/src/DoctorComponents/DoctorEditableRow.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { p_id } from "../Doctor.js";
+import { a_id } from "../Doctor.js";
 
 const DoctorEditableRow = ({editFormData, handleEditFormChange, handleCancelClick}) => {
     //console.log(p_id_arr);
     return (
         <tr>
             <td>{p_id}</td>
+            <td>{a_id}</td>
             <td>
                 <input type="text" required="required" placeholder="Enter feedback..." name = "appt_feedback" value={editFormData.appt_feedback} onChange={handleEditFormChange}></input>
             </td>
--- hospital_management/src/DoctorComponents/DoctorReadOnlyRow.js
+++ hospital_management/src/DoctorComponents/DoctorReadOnlyRow.js
@@ -4,13 +4,14 @@ const DoctorReadOnlyRow = ({patient, handleEditClick, handleDeleteClick}) => {
     return (
         <tr>
             <td>{patient.patient_id}</td>
+            <td>{patient.appt_id}</td>
             <td>{patient.appt_feedback}</td>
             <td>
                 <button type='button' onClick={(event)=>handleEditClick(event, patient)}>Edit</button>
-                <button type='button' onClick={() => handleDeleteClick(patient.patient_id)}>Delete</button>
+                <button type='button' onClick={() => handleDeleteClick(patient.patient_id, patient.appt_id)}>Delete</button>
             </td>
         </tr>
     )
 }
 
-export default DoctorReadOnlyRow
+export default DoctorReadOnlyRow 
